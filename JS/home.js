09ca05016cb7620a36ed5004b83f39ecd6821ac3
Sofia Santa Maria <yolaresofia@gmail.com>
2020-03-05 23:18:54 +0100

    array de fuentes listo

--- JS/home.js
+++ JS/home.js
@@ -11,34 +11,10 @@ let allConejos = [...conejo];
 
 
 
-
-//var rawFonts = ('https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyALN15CEmX5LpdtZyPKYa06Vf63Atm5sXg')
-
-// //MUY IMPORTANTE FUNCION REAL LPM
-// var getFrase = async () => {
-//     console.log('gato clickado');
-//     var response1 = await fetch(`https://www.foaas.com/${randomPos}/${sender.value}`, {
-//         headers: {
-//             Accept: 'application/json'
-//         }
-//     });
-//     var getFraseRaw = await response1.json();
-//     console.log(getFraseRaw);
-//     let testFrase = document.createElement('p');
-//     testFrase.classList.add('frase-tag-p');
-//     testFrase.innerText = getFraseRaw;
-//     conejo.appendChild(testFrase);
-//     let randomNum = Math.floor(Math.random() * 6 + 1);
-//     console.log(randomNum);
-//     randomImg.src = `imagenes/fotos-de-tarjetas-0${randomNum}.png`
-//     randomImg.style.display = 'inline';
-//     closeButton.style.display = 'inline';
-//     console.log(getFraseRaw.message + ' ' + getFraseRaw.subtitle);
-// };
-
 let testFrase = null;
 // //MUY IMPORTANTE FUNCION REAL LPM
 function getFrase(){
+    let arrayOfFonts = ['Pacifico', 'Fredoka One', 'Nosifer', 'Luckiest Guy', 'Courgette', 'Kaushan Script', 'Great Vibes', 'Parisienne'];
     var arrOne = ['asshole', 'awesome', 'bag', 'because', 'bucket', 'bye', 'cool', 'cup', 'diabetes', 'even', 'anyone', 'anything', 'family', 'fascinating', 'flying', 'give', 'holygrail', 'horse', 'idea', 'immensity', 'jinglebells', 'life', 'logs', 'looking', 'maybe', 'me', 'mornin', 'no', 'pink', 'programmer', 'question', 'shit', 'single', 'thanks', 'that', 'this', 'too', 'what', 'zero'];
     var arrOnePos = Math.floor(Math.random() * arrOne.length);
     var randomPos = arrOne[arrOnePos];
@@ -58,6 +34,8 @@ function getFrase(){
         testFrase.classList.add('frase-tag-p');
         data.message += " -" + sender.value;
         testFrase.innerText = data.message;
+        console.log(arrayOfFonts)
+        testFrase.style.fontFamily = arrayOfFonts[random(arrayOfFonts)]
         conejo.appendChild(testFrase);
         let randomNum = Math.floor(Math.random() * 6 + 1);
         randomImg.src = `imagenes/fotos-de-tarjetas-0${randomNum}.png`
@@ -65,6 +43,9 @@ function getFrase(){
         closeButton.style.display = 'inline';
         document.getElementById('senderid').value = "";
     }))
+}
+function random(array) {
+    return Math.floor(Math.random() * array.length-1)
 }
 
 //onclick me da la frase
